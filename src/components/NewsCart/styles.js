8456c29ff2cb523b159e683b10bb1b03d036d378
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-05-10 22:46:54 +0530

    UPDATE!

--- src/components/NewsCart/styles.js
+++ src/components/NewsCart/styles.js
@@ -7,6 +7,22 @@ export default makeStyles({
         width: '100%',
         margin: 0,
   },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '45vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'white'
+  },
+  infoCard : {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center'
+  }
 });
 
  
